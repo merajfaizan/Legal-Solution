c16f8d9c434ae3aef0b1d3ef8f108799eefa62e6
Meraj Faizan <mdmaraj1500@gmail.com>
2022-11-09 14:32:31 +0600

    not found page added

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex justify-center align-middle my-5">
+      <img
+        className="w-full h-96 object-contain"
+        src="https://www.pngitem.com/pimgs/m/254-2549834_404-page-not-found-404-not-found-png.png"
+        alt="notFound"
+      />
+    </div>
+  );
+};
+
+export default NotFound;
--- src/Routes/Router/Router.js
+++ src/Routes/Router/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home";
+import NotFound from "../../Pages/NotFound/NotFound";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "*",
+        element: <NotFound></NotFound>,
       },
     ],
   },
